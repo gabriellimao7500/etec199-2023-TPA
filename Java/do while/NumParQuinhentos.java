ef2dfa78794e3e8ec9e23a5a170757835436e817
gabriellimao7500 <gabriellimao7500@gmail.com>
2023-09-22 09:06:16 -0300

    create do while folder, create for folder

---
+++ Java/do while/NumParQuinhentos.java
@@ -0,0 +1,14 @@
+package gabriel;
+
+public class NumParQuinhentos {
+	public static void main(String[] args) {
+		int i = 0;
+		
+		do {
+			
+			System.out.println(i);
+			i += 2;
+			
+		}while (i <= 500);
+	}
+}
---
+++ Java/for/UsersAge.java
@@ -0,0 +1,30 @@
+package gabriel;
+import java.time.LocalDate;
+import java.util.Scanner;
+public class UsersAge {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int maisVelho = 0, maisNovo = 10000, AnoAtual, idade;
+		
+		AnoAtual = LocalDate.now().getYear();
+		
+		for(int i = 0; i <= 3; i++) {
+			
+			System.out.println("diga sua idade");
+			idade = in.nextInt();
+			
+			idade = AnoAtual - idade;
+			
+			System.out.println("você tem "+idade+" anos");
+			
+			if(idade > maisVelho) {
+				maisVelho = idade;
+			}
+			if(idade < maisNovo) {
+				maisNovo = idade;
+			}
+		}
+		
+		System.out.println("o mais novo tem "+ maisNovo +"anos e o mais velho tem "+ maisVelho +"anos");
+}
+}
